52a91400f13672d1607589a165f3f4bc04fda759
Christian Moura <christianmsbrito@gmail.com>
2021-02-04 11:47:13 -0300

    socket

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div v-for="(msg, i) in messages" :key="i">
+      <div>{{ msg }}</div>
+    </div>
     <form @submit.prevent="send">
       <input type="text" name="message" v-model="message" />
       <button type="submit">Send</button>
@@ -14,6 +17,14 @@ import { Component } from 'vue-property-decorator';
 @Component
 export default class Chat extends Vue {
   message = '';
+
+  messages: string[] = [];
+
+  mounted() {
+    this.$socket.on('receive', (message: string) => {
+      this.messages.push(message);
+    });
+  }
 
   send() {
     this.$socket.emit('message', this.message);
